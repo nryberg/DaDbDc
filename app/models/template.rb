b3cabeb533d367c7c5723cacab5fed592888487c
Nick Ryberg <nryberg@gmail.com>
2014-02-19 23:40:44 -0600

    Added remaining template fields
    Columns and bears oh my

--- app/models/template.rb
+++ app/models/template.rb
@@ -2,5 +2,11 @@ class Template
   include MongoMapper::Document
   key :platform, String
   key :database_name, String
+  key :table_name, String
+  key :column_name, String
+  key :column_type, String
+  key :column_format, String
+  key :column_precision, String
+  key :column_scale, String
 
 end
