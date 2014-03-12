6b3c5b7dc7295f73960de50d18a5033b33b4bf99
Nick Ryberg <nryberg@gmail.com>
2014-03-11 23:05:04 -0500

    Cleaning house

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,13 +1,6 @@
 class Template
   include MongoMapper::Document
   key :platform, String
-  key :database_name, String
-  key :table_name, String
-  key :column_name, String
-  key :column_type, String
-  key :column_format, String
-  key :column_precision, String
-  key :column_scale, String
   key :sample_sql, String
   key :notes, String
 
