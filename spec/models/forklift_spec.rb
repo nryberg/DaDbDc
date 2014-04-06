f6ba847b06037ccf7b02b3041c77bf097fb93121
Nick Ryberg <nryberg@gmail.com>
2014-04-06 17:35:21 -0500

    Starting build out of processing of loaders

--- spec/models/forklift_spec.rb
+++ spec/models/forklift_spec.rb
@@ -10,7 +10,28 @@ describe Forklift do
     end
 
     expect(@forklift.loaders[3].name).to eq("4")
+  end
 
+  it "Should accept real collection of loaders and process" do
+    require 'csv'
+
+    @forklift = Forklift.new
+
+    file_path = "/Users/Nick/Develop/DaDbDc/notes/postgres_sample.csv"
+    infile = File.new(file_path)
+    Loader.upload(infile)
+    #save_and_open_page
+
+    loaders = Loader.all
+
+    loaders.each do |load|
+      @forklift.add_loader(load)
+
+    end
+
+    expect(@forklift.loaders.count).to eq(5)
+
+    @forklift.process_loads
 
   end
 end
